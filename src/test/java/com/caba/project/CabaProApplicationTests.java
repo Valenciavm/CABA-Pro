9828de35f894a5361aae7666db88a73d82f8a33c
Valenciavm <141790853+Valenciavm@users.noreply.github.com>
2025-07-25 16:51:22 -0500

    primer commit

---
+++ src/test/java/com/caba/project/CabaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caba.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
