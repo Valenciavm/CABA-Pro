7a30a54fe25213eef7292b802d203dd2541f251c
CesarMontoyag1 <159492748+CesarMontoyag1@users.noreply.github.com>
2025-08-25 23:12:11 -0500

    Add files via upload

---
+++ CabaPro/src/test/java/com/example/CabaPro/CabaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CabaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
