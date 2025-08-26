39517e837128fdce51540b289d4caf2c65b619f0
CesarMontoyag1 <159492748+CesarMontoyag1@users.noreply.github.com>
2025-08-25 23:12:41 -0500

    Add files via upload

---
+++ src/main/java/com/example/CabaPro/CabaProApplication.java
@@ -0,0 +1,13 @@
+package com.example.CabaPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabaProApplication.class, args);
+	}
+
+}
